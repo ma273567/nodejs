f7b496d588ea1e0e55d4d112efea56a97678c9d3
ma273567 <mazmi@albany.edu>
2018-08-24 21:17:25 -0400

    set up package dependencies

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+    This is a Nodejs project that express and websocket APIs to  try to 'clone' groupme, with a video call 
+    functionality. 
+*/
+
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+
