ac5eb9b52ec1681602c12f2a1debc42a499aa2f6
ma273567 <mazmi@albany.edu>
2018-10-19 18:37:55 -0400

    update and add login page

--- public/js/socket.js
+++ public/js/socket.js
@@ -5,18 +5,24 @@ $(document).ready(function () {
     // Make connection from client side
     var socket = io();
     
-    var emit = function(msg) {
-        socket.emit('chat message', msg);
-    }
+    
 
     /*
     Functions that handles sending messages from clients
     by pressing Enter or clicking send
     Trims white space and checks for null string before emitting the msg
     */
+    var emit = function(msg, user) {
+        
+        var data = {
+            message: msg,
+            user: user
+        }
+        socket.emit('chat message', data);
+    }
     $('#msg').keypress(function(event) {
         var msg = $("#msg").val().trim();  
-        if(event.which == 13 && msg != "" ) {
+        if(event.which == 13 && msg != "") {
             //alert(msg);
             event.preventDefault();
             emit(msg);
